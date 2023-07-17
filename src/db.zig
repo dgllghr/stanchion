3dee4dc951952ffcd61a76d4c90875c1c47a36e1
Dan Gallagher <d@gllghr.net>
2023-07-17 07:33:24 -0400

    database ctx for persistent stmts and migrations

---
+++ src/db.zig
@@ -0,0 +1,128 @@
+const std = @import("std");
+const Type = std.builtin.Type;
+
+const Conn = @import("./sqlite3/Conn.zig");
+const Stmt = @import("./sqlite3/Stmt.zig");
+
+pub const Error = error {
+    QueryReturnedNoRows,
+} || @import("./sqlite3/errors.zig").Error;
+
+pub fn Ctx(comptime statement_fields: []const []const u8) type {
+    var fields: [statement_fields.len + 1]Type.StructField = undefined;
+    fields[0] = .{
+        .name = "conn",
+        .type = Conn,
+        .default_value = null,
+        .is_comptime = false,
+        // TODO what should this be?
+        .alignment = @alignOf(Conn),
+    };
+    inline for (statement_fields, 1..) |f, idx| {
+        fields[idx] = statementField(f);
+    }
+    return @Type(.{
+        .Struct = .{
+            .layout = .Auto,
+            .backing_integer = null,
+            .fields = &fields,
+            .decls = &[_]Type.Declaration{},
+            .is_tuple = false,
+        }
+    });
+}
+
+pub fn deinit_ctx(ctx: anytype) void {
+    const type_info = @typeInfo(@TypeOf(ctx));
+    switch (type_info) {
+        .Struct => |s| {
+            inline for (s.fields) |field| {
+                if (field.type == ?Stmt) {
+                    var stmt = @field(ctx, field.name);
+                    if (stmt) |st| {
+                        st.deinit();
+                    }
+                }
+            }
+        },
+        else => @compileError("db ctx must be a struct"),
+    }
+}
+
+fn statementField(comptime name: []const u8) Type.StructField {
+    return .{
+        .name = name,
+        .type = ?Stmt,
+        .default_value = &@as(?Stmt, null),
+        .is_comptime = false,
+        // TODO what should this be?
+        .alignment = @alignOf(?Stmt),
+    };
+}
+
+pub const Migrations = struct {
+    const setup =
+        \\CREATE TABLE IF NOT EXISTS _stanchion_migrations (
+        \\  version INTEGER NOT NULL PRIMARY KEY,
+        \\  applied_at INTEGER NOT NULL
+        \\) STRICT
+    ;
+
+    const v1 = [_][*:0]const u8 {
+        \\CREATE TABLE _stanchion_tables (
+        \\  id INTEGER NOT NULL PRIMARY KEY,
+        \\  -- It seems like `collate nocase` is what sqlite uses to compare table names
+        \\  -- when referenced in sql queries, so use `collate nocase` on the name column
+        \\  -- to match sqlite's behavior
+        \\  name TEXT NOT NULL COLLATE NOCASE,
+        \\  UNIQUE (name)
+        \\) STRICT
+        ,
+        \\CREATE TABLE _stanchion_columns (
+        \\  table_id INTEGER NOT NULL,
+        \\  rank INTEGER NOT NULL,
+        \\  name TEXT NOT NULL COLLATE NOCASE,
+        \\  column_type TEXT NOT NULL,
+        \\  sk_rank INTEGER NULL,
+        \\  PRIMARY KEY (table_id, rank)
+        \\) STRICT, WITHOUT ROWID
+    };
+
+    const migrations = [_][]const [*:0]const u8 {&v1};
+
+    pub fn apply(conn: Conn) !void {
+        try conn.exec(setup);
+
+        const curr_version = try readCurrentVersion(conn);
+        const insert_migration_stmt = try conn.prepare(
+            \\INSERT INTO _stanchion_migrations (version, applied_at)
+            \\VALUES (?, unixepoch())
+        );
+        defer insert_migration_stmt.deinit();
+        for (migrations[curr_version..], 1..) |m, v| {
+            const version: i32 = @intCast(curr_version + v);
+            // TODO wrap each migration in a savepoint
+            try applyMigration(conn, m);
+
+            try insert_migration_stmt.reset();
+            try insert_migration_stmt.bind(.Int32, 1, version);
+            try insert_migration_stmt.exec();
+        }
+    }
+
+    fn readCurrentVersion(conn: Conn) !usize {
+        const stmt = try conn.prepare(
+            \\SELECT COALESCE(MAX(version), 0) FROM _stanchion_migrations
+        );
+        if (!try stmt.next()) {
+            return Error.QueryReturnedNoRows;
+        }
+        return @intCast(stmt.read(.Int32, false, 0));
+    }
+
+    fn applyMigration(conn: Conn, migration: []const [*:0]const u8) !void {
+        for (migration) |stmt| {
+            try conn.exec(stmt);
+        }
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,12 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 const c = @import("./sqlite3/c.zig").c;
+const sqlite_errors = @import("./sqlite3/errors.zig");
 const vtab = @import("./sqlite3/vtab.zig");
 
+const Conn = @import("./sqlite3/Conn.zig");
+
+const Migrations = @import("./db.zig").Migrations;
 const Table = @import("./Table.zig");
 const StanchionVTab = vtab.VirtualTable(Table);
 
@@ -12,14 +16,18 @@ pub export fn sqlite3_stanchion_init(
     err_msg: [*c][*c]u8,
     api: *c.sqlite3_api_routines,
 ) callconv(.C) c_int {
-    _ = err_msg;
-
     c.sqlite3_api = api;
 
     // To store data common to all table instances (global to the module), replace this
     // allocator with a struct containing the common data (see `ModuleContext` in
     // `zig-sqlite`)
     var allocator = std.heap.GeneralPurposeAllocator(.{}){};
+
+    const conn = Conn.init(db);
+    Migrations.apply(conn) catch {
+        err_msg.* = @constCast(@ptrCast("error apply migrations"));
+        return c.SQLITE_ERROR;
+    };
 
     const res = c.sqlite3_create_module_v2(
         db,
@@ -29,9 +37,8 @@ pub export fn sqlite3_stanchion_init(
         null
     );
     if (res != c.SQLITE_OK) {
-        // TODO return proper error
-        //return errors.errorFromResultCode(result);
-        return -1;
+        err_msg.* = @constCast(@ptrCast("error creating module"));
+        return res;
     }
 
     return c.SQLITE_OK;
