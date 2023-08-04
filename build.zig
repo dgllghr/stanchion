3353a2c5b38ae9855bb0e0473f51683dc8d87a17
Dan Gallagher <d@gllghr.net>
2023-08-04 12:33:24 -0400

    adjust build.zig for latest zig

--- build.zig
+++ build.zig
@@ -15,7 +15,6 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-
     const lib = b.addStaticLibrary(.{
         .name = "stanchion",
         // In this case the main source file is merely a path, however, in more
@@ -24,8 +23,11 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    lib.addCSourceFile("src/sqlite3/c/sqlite3.c", &[_][]const u8{"-std=c99"});
-    lib.addIncludePath("src/sqlite3/c");
+    lib.addCSourceFile(.{
+        .file = .{ .path = "src/sqlite3/c/sqlite3.c" },
+        .flags = &[_][]const u8{"-std=c99"},
+    });
+    lib.addIncludePath(.{ .path = "src/sqlite3/c" });
     lib.linkLibC();
     const lib_options = b.addOptions();
     lib.addOptions("build_options", lib_options);
@@ -38,7 +40,6 @@ pub fn build(b: *std.Build) void {
     // running `zig build`).
     b.installArtifact(lib);
 
-
     const loadable_ext = b.addSharedLibrary(.{
         .name = "stanchion",
         .root_source_file = .{ .path = "src/main.zig" },
@@ -46,7 +47,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     loadable_ext.force_pic = true;
-    loadable_ext.addIncludePath("src/sqlite3/c");
+    loadable_ext.addIncludePath(.{ .path = "src/sqlite3/c" });
     // TODO add anonymous module?
     //      https://github.com/vrischmann/zig-sqlite/blob/193240aa5ecc9c9b14b25dec662505d5cf9f5340/build.zig#L350
     loadable_ext.linkLibrary(lib);
@@ -54,11 +55,10 @@ pub fn build(b: *std.Build) void {
     loadable_ext.addOptions("build_options", loadable_ext_options);
     loadable_ext_options.addOption(bool, "loadable_extension", true);
 
-    const install_loadable_ext = b.addInstallArtifact(loadable_ext);
+    const install_loadable_ext = b.addInstallArtifact(loadable_ext, .{});
 
     const loadable_ext_build = b.step("ext", "Build the 'stanchion' sqlite loadable extension");
     loadable_ext_build.dependOn(&install_loadable_ext.step);
-
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
@@ -67,17 +67,19 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    main_tests.addCSourceFile("src/sqlite3/c/sqlite3.c", &[_][]const u8{"-std=c99"});
-    main_tests.addIncludePath("src/sqlite3/c");
+    main_tests.addCSourceFile(.{
+        .file = .{ .path = "src/sqlite3/c/sqlite3.c" },
+        .flags = &[_][]const u8{"-std=c99"},
+    });
+    main_tests.addIncludePath(.{ .path = "src/sqlite3/c" });
     const main_tests_options = b.addOptions();
     main_tests.addOptions("build_options", main_tests_options);
     main_tests_options.addOption(bool, "loadable_extension", false);
 
     const run_main_tests = b.addRunArtifact(main_tests);
 
-
     const benches = b.addExecutable(.{
-        .name ="stanchion_benches",
+        .name = "stanchion_benches",
         .root_source_file = .{ .path = "src/bench.zig" },
         .target = target,
         // This may be too aggressive but the speed up is significant for scanning the
